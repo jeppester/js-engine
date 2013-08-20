96756bc511b674d905ecaba229027697b17d41f9
Jesper Sørensen <jeppester1@gmail.com>
2013-08-20 17:00:01 +0200

    Missing files from last commit

---
+++ js/jsEngine/Engine/Globals.js
@@ -0,0 +1,77 @@
+/**
+ * jseGlobals.js:
+ * This file contains global JsEngine variables.
+ * The purpose of the global variables is to be used as readable alternatives to "magic numbers".
+ */
+
+// Keyboard keys
+KEY_LEFT		= 37;
+KEY_UP			= 38;
+KEY_RIGHT		= 39;
+KEY_DOWN		= 40;
+KEY_SPACE		= 32;
+
+KEY_BACKSPACE	= 8;
+KEY_TAB			= 9;
+KEY_ENTER		= 13;
+KEY_SHIFT		= 16;
+KEY_CONTROL		= 17;
+KEY_ALT_LEFT	= 18;
+KEY_CAPSLOCK	= 20;
+KEY_ESCAPE		= 27;
+KEY_ALT_RIGHT	= 0;
+
+KEY_F1			= 112;
+KEY_F2			= 113;
+KEY_F3			= 114;
+KEY_F4			= 115;
+KEY_F5			= 116;
+KEY_F6			= 117;
+KEY_F7			= 118;
+KEY_F8			= 119;
+KEY_F9			= 120;
+KEY_F10			= 121;
+KEY_F11			= 122;
+KEY_F12			= 123;
+
+// Pointers
+MOUSE_ANY		= 0;
+MOUSE_1			= 1;
+MOUSE_2			= 2;
+MOUSE_3			= 3;
+MOUSE_4			= 4;
+MOUSE_5			= 5;
+MOUSE_6			= 6;
+MOUSE_7			= 7;
+MOUSE_8			= 8;
+MOUSE_9			= 9;
+MOUSE_10		= 10;
+
+TOUCH_ANY		= 20;
+TOUCH_1			= 21;
+TOUCH_2			= 22;
+TOUCH_3			= 23;
+TOUCH_4			= 24;
+TOUCH_5			= 25;
+TOUCH_6			= 26;
+TOUCH_7			= 27;
+TOUCH_8			= 28;
+TOUCH_9			= 29;
+TOUCH_10		= 30;
+
+MOUSE_TOUCH_ANY = 100;
+
+// Speed units
+SPEED_PIXELS_PER_SECOND		= 1;
+SPEED_PIXELS_PER_FRAME		= 2;
+
+// Offset options
+OFFSET_TOP_LEFT 			= 'tl';
+OFFSET_TOP_CENTER			= 'tc';
+OFFSET_TOP_RIGHT			= 'tr';
+OFFSET_MIDDLE_LEFT			= 'ml';
+OFFSET_MIDDLE_CENTER		= 'mc';
+OFFSET_MIDDLE_RIGHT			= 'mr';
+OFFSET_BOTTOM_LEFT			= 'bl';
+OFFSET_BOTTOM_CENTER		= 'bc';
+OFFSET_BOTTOM_RIGHT			= 'br';
---
+++ js/jsEngine/autoload.js
@@ -0,0 +1,76 @@
+// Load necessary files without making a mess
+(function () {
+	var loadFiles, scripts, enginePath;
+
+	// Create file loader function
+	loadFiles = function (filePaths) {
+		var i, req, codeString; /*script, parent;*/
+
+		if (typeof filePaths === "string") {
+			filePaths = [filePaths];
+		}
+
+		for (i = 0; i < filePaths.length; i ++) {
+			req = new XMLHttpRequest();
+			req.open('GET', filePaths[i], false);
+			req.send();
+			codeString = req.responseText + "\n//@ sourceURL=/" + filePaths[i];
+			try {
+				eval(codeString);
+			}
+			catch (e) {
+				console.log('Failed loading "' + filePaths[i]);
+				throw new Error(e);
+			}
+		}
+
+		if (window.loadedFiles === undefined) {window.loadedFiles = []; }
+		window.loadedFiles = window.loadedFiles.concat(filePaths);
+	};
+
+	// Find file path of autoload script
+	scripts = document.getElementsByTagName("script");
+	enginePath = scripts[scripts.length-1].src.replace(/\/autoload\.js$/, '');
+
+	// Load all engine files
+	loadFiles([
+		enginePath + '/Polyfill/requestAnimationFrame.js',
+
+		enginePath + '/Extension/Array.js',
+		enginePath + '/Extension/Object.js',
+
+		enginePath + '/Engine/Class.js',
+		enginePath + '/Engine/Globals.js',
+		enginePath + '/Engine/Engine.js',
+		enginePath + '/Engine/Loader.js',
+
+		enginePath + '/Lib/Animatable.js',
+
+		enginePath + '/Math/Vector.js',
+	    enginePath + '/Math/Line.js',
+	    enginePath + '/Math/Circle.js',
+	    enginePath + '/Math/Rectangle.js',
+	    enginePath + '/Math/Polygon.js',
+
+	    enginePath + '/View/Child.js',
+	    enginePath + '/View/Line.js',
+	    enginePath + '/View/Circle.js',
+	    enginePath + '/View/Rectangle.js',
+	    enginePath + '/View/Polygon.js',
+	    enginePath + '/View/Container.js',
+	    enginePath + '/View/Sprite.js',
+	    enginePath + '/View/Collidable.js',
+	    enginePath + '/View/TextBlock.js',
+	    enginePath + '/View/GameObject.js',
+
+	    enginePath + '/Engine/Room.js',
+	    enginePath + '/Engine/Camera.js',
+	    enginePath + '/Engine/CustomLoop.js',
+
+	    enginePath + '/Input/Keyboard.js',
+		enginePath + '/Input/Pointer.js',
+		
+		enginePath + '/Sound/Effect.js',
+		enginePath + '/Sound/Music.js',
+	]);
+})();
